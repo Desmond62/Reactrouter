54573979adf61183d29ed6707571983fa680d885
Desmond62 <desmondsolomon623@gmail.com>
2023-11-01 07:44:31 -0700

    start

---
+++ src/Components/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Index() {
+  return (
+    <div>
+      <h1>My HomePage is loading</h1>
+    </div>
+  )
+}
+
+export default Index
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Index from '../Components/Index'
+
+function Home() {
+  return (
+    <div>
+    <Index/>  
+    </div>
+  )
+}
+
+export default Home
